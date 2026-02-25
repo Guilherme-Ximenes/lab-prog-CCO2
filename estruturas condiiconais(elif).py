da3dc7c6150ac69c2c2fb02aea0eae5e84f56102
Guilherme <gsbximenes06@gmail.com>
2026-02-25 13:02:44 +0000

    uso do elif

--- estruturas condiiconais(elif).py
+++ estruturas condiiconais(elif).py
@@ -2,8 +2,8 @@ a = input()
 b = input()
 
 if a > b:
-    print(f"{a} é maior que {b}")
+    print(f"{a} é maior que {b}!")
 elif a == b:
-    print(f"{a} é igual a {b}")
+    print(f"{a} é igual a {b}!")
 else:
-    print(f"{a} é menor que {b}")
+    print(f"{a} é menor que {b}!")
