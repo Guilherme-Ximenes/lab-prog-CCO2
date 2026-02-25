6943074c9c62d164655384317092e705cc7b5728
Guilherme <gsbximenes06@gmail.com>
2026-02-25 12:22:56 +0000

    Utilizando melhor o else

--- estruturas condicionais.py
+++ estruturas condicionais.py
@@ -1,6 +1,6 @@
-a = 33
+a = 300
 b = 200
-idade = 18
+idade = 15
 
-print("b é maior que a" if b > a else None)
+print("b é maior que a" if b > a else "b é menor que a")
 print("Você é maior de idade!" if idade >= 18 else "Você é menor de idade!")
