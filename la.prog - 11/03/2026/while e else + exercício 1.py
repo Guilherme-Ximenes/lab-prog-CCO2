f4443c29dcac0f2de2146ea0588f590a418d3619
Guilherme <gsbximenes06@gmail.com>
2026-03-11 12:14:21 +0000

    else no while

---
+++ la.prog - 11/03/2026/while e else + exercício 1.py
@@ -0,0 +1,17 @@
+'''
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+else:
+    print("i não é mais menor ou igual a 5!")
+'''
+
+pedido = 0
+
+while pedido < 4:
+    pedido += 1
+    print(f"Separando pedido {pedido}")
+else:
+    print("Todos os pedidos foram separados!")
