40823e2bbd1d0723cf09d612556871eb3003735a
Guilherme Ximenes <gsbximenes06@gmail.com>
2026-05-12 23:21:41 -0300

    sequencia fibonacci

---
+++ fibonacci-teste.py
@@ -0,0 +1,17 @@
+#Fibonacci
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    sequence = [0, 1]
+    for i in range(2, n):
+        next_number = sequence[i-1] + sequence[i-2]
+        sequence.append(next_number)
+    
+    return sequence
+
+print(fibonacci(10))
